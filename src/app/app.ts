90b62d2c459cf23a7a54a4544f5787782a62f889
Daniele Inspiron <contatodanny@hotmail.com>
2025-10-19 20:55:44 -0300

    primeiro commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('gerador-certificado');
+ title = 'gerador-certificado';
+ exibeNavbar: boolean = true;
 }
