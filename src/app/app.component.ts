5730e3419cde340e69af4b1381db246d38f21447
Daniele Inspiron <contatodanny@hotmail.com>
2025-10-27 17:01:26 -0300

    item-certificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { Navbar } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, RouterOutlet, CommonModule, PrimaryButtonComponent, SecondaryButton],
+  imports: [Navbar, RouterOutlet, CommonModule, PrimaryButtonComponent, SecondaryButton, ItemCertificado],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
